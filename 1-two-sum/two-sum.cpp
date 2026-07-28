0caed1afd67b0b5348a4707ed7c905ef3d9ee250
Parth Padmawar <parthpadmawar4@gmail.com>
2026-07-28 10:45:27 +0530

    Time: 43 ms (24.14%) | Memory: 14.1 MB (83.41%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        int i;
+        int j;
+        int n = nums.size();
+        for (i = 0; i < n; i++) {
+            for (j = i+1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return {};
+    }
+};
