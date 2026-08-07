febaca7d254c2b84fc1ef94060b85cf5f6a09e64
Parth Padmawar <parthpadmawar4@gmail.com>
2026-08-07 17:57:55 +0530

    Time: 3 ms (69.99%) | Memory: 45.7 MB (97.95%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,31 +1,17 @@
 class Solution {
+    boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
     public String reverseVowels(String s) {
         StringBuilder sb = new StringBuilder(s);
         int z;
         int j = s.length()-1;
         int a = j;
         for(z=0;z<a;z++){
-            if (sb.charAt(z) == 'a' ||
-                sb.charAt(z) == 'e' ||
-                sb.charAt(z) == 'i' ||
-                sb.charAt(z) == 'o' ||
-                sb.charAt(z) == 'u' ||
-                sb.charAt(z) == 'A' ||
-                sb.charAt(z) == 'E' ||
-                sb.charAt(z) == 'I' ||
-                sb.charAt(z) == 'O' ||
-                sb.charAt(z) == 'U'){
+            if (isVowel(sb.charAt(z))){
                     for(j=a;j>z;j--){
-                        if (sb.charAt(j) == 'a' ||
-                            sb.charAt(j) == 'e' ||
-                            sb.charAt(j) == 'i' ||
-                            sb.charAt(j) == 'o' ||
-                            sb.charAt(j) == 'u' ||
-                            sb.charAt(j) == 'A' ||
-                            sb.charAt(j) == 'E' ||
-                            sb.charAt(j) == 'I' ||
-                            sb.charAt(j) == 'O' ||
-                            sb.charAt(j) == 'U'){
+                        if (isVowel(sb.charAt(j))){
                                 char temp = sb.charAt(z);
                                 sb.setCharAt(z,sb.charAt(j));
                                 sb.setCharAt(j,temp);
