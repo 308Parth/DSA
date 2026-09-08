27402ed81747eec89c0ada9222b83e66572a522e
Parth Padmawar <parthpadmawar4@gmail.com>
2026-09-08 10:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (38.58%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,8 +8,10 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        /*
         ListNode dummy = new ListNode();
         ListNode cur = dummy;
 
@@ -26,6 +28,40 @@ class Solution {
 
         cur.next = (list1 != null) ? list1 : list2;
 
-        return dummy.next;        
+        return dummy.next;
+        */
+
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        ListNode newList = new ListNode(0);
+        ListNode head = newList;
+
+        while (temp1 != null && temp2 != null) {
+
+            if (temp1.val < temp2.val) {
+                newList.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                newList.next = temp2;
+                temp2 = temp2.next;
+            }
+
+            newList = newList.next;
+        }
+
+        while (temp1 != null) {
+            newList.next = temp1;
+            temp1 = temp1.next;
+            newList = newList.next;
+        }
+
+        while (temp2 != null) {
+            newList.next = temp2;
+            temp2 = temp2.next;
+            newList = newList.next;
+        }
+
+        return head.next;        
     }
 }
